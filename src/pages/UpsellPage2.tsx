9f56e799aaccc34a02163cb1c62bee131d386865
Criação ND Hub <dev@criacaondhub.com>
2026-02-27 19:52:03 -0300

    feat: duplicate upsell page and add route for /presente2

---
+++ src/pages/UpsellPage2.tsx
@@ -0,0 +1,58 @@
+import UpsellHero from "../components/sections/UpsellHero"
+import UpsellMethod from "../components/sections/UpsellMethod"
+import UpsellForWhom from "../components/sections/UpsellForWhom"
+import UpsellModules from "../components/sections/UpsellModules"
+import UpsellBonus from "../components/sections/UpsellBonus"
+import Testimonials from "../components/sections/Testimonials"
+import Guarantee from "../components/sections/Guarantee"
+import UpsellPricing from "../components/sections/UpsellPricing"
+import About from "../components/sections/About"
+import FAQ from "../components/sections/FAQ"
+import FloatingWhatsApp from "../components/FloatingWhatsApp"
+import Footer from "../components/sections/Footer"
+import LiquidEther from "../components/ui/LiquidEther"
+
+function UpsellPage2() {
+    return (
+        <main className="relative min-h-screen bg-transparent">
+            {/* Background Liquid Effect - Page Wide, Desktop Only */}
+            <div className="hidden lg:block fixed inset-0 z-0 pointer-events-none">
+                <LiquidEther
+                    colors={['#e31c1c', '#761414', '#cc0000']}
+                    mouseForce={40}
+                    cursorSize={100}
+                    isViscous={true}
+                    viscous={30}
+                    iterationsViscous={32}
+                    iterationsPoisson={32}
+                    resolution={0.5}
+                    isBounce={false}
+                    autoDemo={true}
+                    autoSpeed={0.5}
+                    autoIntensity={2.2}
+                    takeoverDuration={0.25}
+                    autoResumeDelay={3000}
+                    autoRampDuration={0.6}
+                />
+            </div>
+
+            <div className="relative z-10">
+                <UpsellHero />
+                <UpsellMethod />
+                <UpsellForWhom />
+                <UpsellModules />
+                <UpsellBonus />
+                <Testimonials />
+                <Guarantee />
+                <UpsellPricing />
+                <About />
+                <FAQ />
+
+                <FloatingWhatsApp />
+                <Footer />
+            </div>
+        </main>
+    )
+}
+
+export default UpsellPage2
