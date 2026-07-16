69bb5d8db65b2ff5f2de6d89163bb2ef8845418c
fesmoraes97 <fesmoraes97@gmail.com>
2026-07-16 19:24:11 -0300

    ajuste numero

--- src/components/FloatingWhatsApp.tsx
+++ src/components/FloatingWhatsApp.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 const FloatingWhatsApp = () => {
     return (
         <motion.a
-            href="https://api.whatsapp.com/send/?phone=5511936237310&text=Olá%21+vim+da+página+do+Oratória+de+Elite+e+quero+saber+mais.&type=phone_number&app_absent=0"
+            href="https://api.whatsapp.com/send/?phone=5511936234882&text=Olá%21+vim+da+página+do+Oratória+de+Elite+e+quero+saber+mais.&type=phone_number&app_absent=0"
             target="_blank"
             rel="noopener noreferrer"
             initial={{ opacity: 0, scale: 0, y: 20 }}
